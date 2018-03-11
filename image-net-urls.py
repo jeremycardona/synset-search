de5151a28878eeb5dca6b137f98b66c0b39d32df
Jeremy Cardona <jeremy.cardona@hotmail.com>
2018-03-10 20:18:48 -0400

    synset search and return a list of urls by the synset id

--- image-net-urls.py
+++ image-net-urls.py
@@ -2,56 +2,52 @@
 import json 
 import re
 import requests
-import urllib
+from bs4 import BeautifulSoup
+import urllib.request
 
 class SynsetSearch:
     'search for synset url of images, input the keyword to search for in the api. Returns a list of urls for the keyword'
-    
-    synset = [] # list of synsets 
-    synsetDict = {
-        "keyword": {
-            "_id": int,
-            "_value": str
-        },
-        "keyword2": {
-            "_id": int,
-            "_value": str
-        }
-    } # synset dictionary of id and name
-    
-    
-    def __init__(self, keyword):
-        self.synset.append(keyword)
+    synset = []
+    synsetDict = {}
+    def __init__(self):
+        pass
 
     def addKeyword(self, keyword):
-        self.synset.append(keyword)
-
-    def getUrl(self, keyword):
-        url = "http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=" + self.synsetDict.keyword._id
-        return url
-
-    def searchKeywords(self, keyword):
-        wordsFile  = urllib.urlopen("http://image-net.org/archive/words.txt") # search query using keyword
-        for line in wordsFile:
-            k = line.slit('\t')
-            v = 
-
-        return 
+        pass
         
-    
-    #def getUrls(self, synsetId):
+    def getUrl(self, keyword):
+        pass
+        
+    def getKeywordIds(self, keyword):
+        wordsFile  = urllib.request.urlopen("http://image-net.org/archive/words.txt")
+        for line in wordsFile:
+            temp = line.decode("utf-8")
+            if keyword + "," in temp:
+                self.synset.append(temp)
+        return self.synset
+        
+    def getDict(self):
+        rows = ( line.split("\t") for line in self.synset )
+        self.synsetDict = { row[0]:row[1:] for row in rows }
+        return self.synsetDict
+        
+                
+    def getUrls(self, synsetId):
+        urlFile = urllib.request.urlopen("http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=" + synsetId)
+        soup = BeautifulSoup(urlFile, "html.parser" ).decode('UTF-8')
+        urlList = soup.split()
+        return urlList
 
 #initialize images
-images = SynsetSearch("dog")
+images = SynsetSearch()
 
-images.addKeyword("cat")
+#returns a list of all the ids that has the dog keyword
+dogId = images.getKeywordIds("dog")
+#returns a dictionary of id, values 
+dogDict = images.getDict()
 
-images.getUrl("dog")
+#get list of keys with dogDict.keys()
+#print(list(dogDict.keys())[0]) this returns the first key in the dictionary
+dogUrls = images.getUrls(list(dogDict.keys())[0])
+print(dogUrls)
 
-
-
-#print(Cars.query("car"))
-#link = "http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n02472987"
-#f = urllib.urlopen(link)
-#myfile = f.read()
-#print myfile
